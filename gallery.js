7d6632956f6dc0a04220d52e255d1a3b1cc50930
foysal777 <foysal.cse11@gmail.com>
2025-04-26 20:17:37 +0600

    add backend api for gallery images

---
+++ gallery.js
@@ -0,0 +1,9 @@
+const apiUrl = "https://car-repair-backend-drf.vercel.app/blog/gallery/"
+const galleryGrid = document.getElementById("gallery-grid");
+const filterButton = document.getElementById("filter-buttons");
+
+let galleryData = []
+
+async function GalleryData(params) {
+    
+}
